525a7fe2392054584dbcc88a263bed9738d90666
Burman Bedford <burmanbed@gmail.com>
2023-08-11 20:33:31 -0400

    created the functionality of the input field and the detection button to render the image based on the url

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,70 @@ import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
 import './App.css'
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
 
+const returnClarifaiRequestOptions= (imageURL) =>{
+  const PAT = 'f74cfa233fe046859640c19a25ce1159';
 
+  const USER_ID = 'burmanbed';       
+  const APP_ID = 'Smart-Brain-App';
+
+  const MODEL_ID = 'face-detection';  
+  const IMAGE_URL = imageURL;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+
+return requestOptions
+}
 
 
 class App extends Component {
- constructor(){
-  super();
-  this.state = {
-    input: ''
-  }
-  }
-onInputChange = (event) =>{
-  console.log(event.target.value)
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageURL: ''
+    }
+    }
+onInputChange = (event) => {
+  this.setState({input: event.target.value})
 }
 
 onButtonSubmit = () => {
-  console.log('click')
+  this.setState({imageURL: this.state.input})
+  fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", returnClarifaiRequestOptions(this.state.input))
+  .then(response => response.json())
+  // app.models.predict('ac547022a07a4d7bb1e35f661554bd04', 'https://etcanada.com/wp-content/uploads/2023/04/GettyImages-1485003120.jpg?quality=80&strip=all&w=720&h=480&crop=1').then(
+  //   function(response){
+  //     console.log(response)
+  //   },
+  //   function(err){
+
+  //   }
+  // )
 }
 
 render() {
@@ -34,7 +81,9 @@ render() {
         onInputChange = { this.onInputChange } 
         onButtonSubmit = { this.onButtonSubmit }
         />
-      {/* <FaceRecognition /> */}
+      <FaceRecognition 
+      imageURL = {this.state.imageURL}
+      /> 
     </>
   )
 }
---
+++ src/Components/FaceRecognition/FaceRecognition.jsx
@@ -0,0 +1,11 @@
+
+
+const FaceRecognition = ({imageURL}) => {
+    return(
+        <div className="center ma">
+            <img src={imageURL} alt=""  width='500px' height='auto'/>
+        </div>
+    )
+}
+
+export default FaceRecognition
